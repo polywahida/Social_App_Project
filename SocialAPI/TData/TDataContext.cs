0f72b5c2a29141ea08d819eecb76f7099ac08c76
poly_wahida <ifrahpoly@gmail.com>
2023-09-22 01:14:41 +0600

    Project Skeleton part-1

---
+++ SocialAPI/TData/TDataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SocialAPI.TEntities;
+
+namespace SocialAPI.TData
+{
+    public class TDataContext : DbContext
+    {
+        public TDataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
+
